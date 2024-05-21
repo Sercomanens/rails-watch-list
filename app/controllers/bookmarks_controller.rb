b5b8c07e0ff9787f9b19974561e8fa3876379556
Laurent Sercomanens <laurent.sercomanens@gmail.com>
2024-05-21 16:29:58 +0530

    modify controllers and routes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,27 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[list:id])
+    @movie = Movie.find(params[movie:id])
+
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+
+
+    if @bookmark.save
+      redirect_to bookmarks_path(@bookmark)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :bookmarks do
-    resources :lists
-  end
-
-  resources :lists, only: [:destroy]
+  resources :bookmarks
+  resources :lists
 end
