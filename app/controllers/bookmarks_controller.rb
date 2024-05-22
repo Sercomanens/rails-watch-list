de4cf5d612293169f79154e303fb797af14c6a95
Laurent Sercomanens <laurent.sercomanens@gmail.com>
2024-05-22 21:22:33 +0530

    modify files

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -17,23 +17,27 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    respond_to do |format|
-      if @bookmark.save
-        format.turbo_stream
-        format.html { redirect_to bookmarks_path, notice: 'Bookmark was successfully created.' }
-      else
-        format.turbo_stream { render turbo_stream: turbo_stream.replace(@bookmark, partial: 'bookmarks/form', locals: { bookmark: @bookmark }) }
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @bookmark.save
+      redirect_to bookmarks_path, notice: 'Bookmark was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-    def destroy
-      @bookmark = Bookmark.find(params[:id])
-      @bookmark.destroy
-      redirect_to bookmarks_path, notice: 'Bookmark was successfully deleted.'
+  def update
+    @bookmark = Bookmark.find(params[:id])
+    if @bookmark.update(bookmark_params)
+      redirect_to bookmarks_path, notice: 'Bookmark was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmarks_path, notice: 'Bookmark was successfully deleted.'
+  end
 
   private
 
