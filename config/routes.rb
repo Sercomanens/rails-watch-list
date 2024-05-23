597825a9ce5259cd0ac0b175daecd30c78dc66d8
Laurent Sercomanens <laurent.sercomanens@gmail.com>
2024-05-23 12:51:52 +0530

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :bookmarks, only: [:index, :new, :create, :destroy, :show, :edit, :update]
-  resources :lists
-  resources :movies
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
